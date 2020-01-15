d2d647b75fd5672df93c4d11cef08d6a5596f891
tonghua711 <tonghua711@163.com>
2020-01-16 00:54:06 +0800

    递交 smtplib 练习模块代码，每日提交

---
+++ csv/readCVS3.py
@@ -0,0 +1,4 @@
+import pandas
+
+df = pandas.read_csv('。')
+print(df)
---
+++ 爬虫/gzDay/1.url.py
@@ -0,0 +1,30 @@
+#encoding:utf-8
+
+# import urllib.request
+# url = "http://www.baidu.com"
+# response = urllib.request.urlopen(url)
+# code = response.getcode()
+# html = response.read()
+# mystr = html.decode("utf-8")
+# response.close()
+# print(mystr)
+
+import urllib.request
+def download1(url):
+    return urllib.request.urlopen(url).read()  #读取全部网页
+
+def download2(url):
+    return urllib.request.urlopen(url).readlines()  #读取网页的每一行，压入列表
+
+def download3(url):
+    response = urllib.request.urlopen(url)   #网页抽象为文件
+    while True:
+        line = response.readline()  #读取一行
+        if not line:
+            break
+
+
+url="http://www.baidu.com"
+# print(download1(url).decode("utf-8"))
+# print(download2(url).decode("utf-8"))
+print(download3(url))
